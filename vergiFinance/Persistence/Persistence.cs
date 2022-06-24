9c0f800f6ee8df0ffe11ca3f0b7b10bf3d55ddcd
Teemu Laine <teemu.laine@etteplan.com>
2022-06-24 10:18:52 +0300

    Add basic LiteDB staking load and save

---
+++ vergiFinance/Persistence/Persistence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace vergiFinance.Persistence
+{
+    public class Persistence
+    {
+        private readonly string _dbName;
+
+        public Persistence(string dbName = "staking.db")
+        {
+            _dbName = dbName;
+        }
+
+        public bool TryLoadSingleStakingData(string ticker, DateTime tradeDate, out StakingDto data)
+        {
+            data = new StakingDto();
+            using (var db = new LiteDatabase(_dbName))
+            {
+                var collection = db.GetCollection<StakingDto>("staking");
+
+                var results = collection.Query()
+                    .Where(x => x.Ticker == ticker && x.TradeDate.Date.Equals(tradeDate.Date))
+                    .ToList();
+
+                if (results == null) return false;
+                if (results.Count > 1)
+                {
+                    throw new ArgumentException($"More than one entry found for [{ticker}, {tradeDate.Date}]");
+                }
+
+                data = results.Single();
+                return true;
+            }
+        }
+
+        public void SaveSingleStakingData(StakingDto dto)
+        {
+            using (var db = new LiteDatabase(_dbName))
+            {
+                var collection = db.GetCollection<StakingDto>("staking");
+                collection.Insert(dto);
+            }
+        }
+
+        public void ClearCollection(string collectionName = "staking")
+        {
+            using (var db = new LiteDatabase(_dbName))
+            {
+                db.DropCollection(collectionName);
+            }
+        }
+    }
+}
---
+++ vergiFinance/Persistence/StakingDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vergiFinance.Persistence
+{
+    public class StakingDto
+    {
+        /// <summary>
+        /// Nordnet: Nro
+        /// Kraken: Id
+        /// </summary>
+        public string Id { get; set; } = "";
+
+        /// <summary>
+        /// NOK, NVDA, ETH, 
+        /// </summary>
+        public string Ticker { get; set; } = "";
+        
+        /// <summary>
+        /// When trade occured
+        /// </summary>
+        public DateTime TradeDate { get; set; }
+        
+        /// <summary>
+        /// Ticker unit price in midday of <see cref="TradeDate"/>
+        /// </summary>
+        public decimal DayUnitPrice { get; set; }
+    }
+}
