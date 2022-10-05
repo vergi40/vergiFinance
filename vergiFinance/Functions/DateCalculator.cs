91a68260413e4c38e1d8892fcee9216fb33902cb
Teemu Laine <teemu.laine@laide.fi>
2022-10-05 22:31:23 +0300

    Add Functions-subcategory. Implement due date calculation

---
+++ vergiFinance/Functions/DateCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vergiFinance.Functions
+{
+    internal class DateCalculator
+    {
+        public DateOnly CalculateDueDate(int paymentPeriodDays)
+        {
+            var currentDate = DateTime.Now;
+            var dueDate = currentDate.AddDays(paymentPeriodDays);
+
+            return DateOnly.FromDateTime(dueDate);
+        }
+    }
+}
--- vergiFinance/General.cs
+++ vergiFinance/General.cs
@@ -1,4 +1,6 @@
-﻿using vergiFinance.Brokers;
+﻿using System.Text;
+using vergiFinance.Brokers;
+using vergiFinance.Functions;
 
 namespace vergiFinance
 {
@@ -6,12 +8,33 @@ namespace vergiFinance
 
     public static class General
     {
+        /// <summary>
+        /// Generates event collection entity, which can be used in various sales calculations and reports
+        /// </summary>
+        /// <param name="lines"></param>
+        /// <returns></returns>
         public static IEventLog ReadKrakenTransactions(IReadOnlyList<string> lines)
         {
             // TODO IoC binding
             IBrokerService broker = new KrakenBroker();
 
             return broker.ReadTransactions(lines);
+        }
+
+        /// <summary>
+        /// Use current date to calculate due date for given payment period.
+        /// </summary>
+        /// <returns>Print containing relevant info</returns>
+        public static string CalculateDueDate(int paymentPeriodDays)
+        {
+            var dateCalculator = new DateCalculator();
+            var dueDate = dateCalculator.CalculateDueDate(paymentPeriodDays);
+
+
+            var messageBuilder = new StringBuilder($"---");
+            messageBuilder.AppendLine($"Current date: {DateTime.Now}");
+            messageBuilder.AppendLine($"Due date with {paymentPeriodDays} days payment period: {dueDate}");
+            return messageBuilder.ToString();
         }
     }
 }
