158470c1be630e6c17d64e47fda9932931e1fd62
Teemu Laine <teemu.laine@laide.fi>
2023-05-06 23:47:20 +0300

    Fix WorkDaysCalculator visibility

--- vergiFinance/Functions/WorkDaysCalculator.cs
+++ vergiFinance/Functions/WorkDaysCalculator.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PublicHoliday;
+﻿using PublicHoliday;
 
 namespace vergiFinance.Functions
 {
-    internal class WorkDaysCalculator
+    public class WorkDaysCalculator
     {
         /// <summary>
         /// 
