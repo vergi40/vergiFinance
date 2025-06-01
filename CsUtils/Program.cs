e5524eb7e87d4636d579f41742e11bdf99a2b9f0
Teemu Laine <teemu.laine@laide.fi>
2025-06-01 14:10:15 +0300

    Add ConsoleAppFramework and AOT

--- CsUtils/Program.cs
+++ CsUtils/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace CsUtils
+﻿using ConsoleAppFramework;
+
+namespace CsUtils;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // https://github.com/Cysharp/ConsoleAppFramework
+        var app = ConsoleApp.Create();
+        app.Add<Commands>();
+        app.Run(args);
     }
+}
+
+public class Commands
+{
+    /// <summary>Display message.</summary>
+    /// <param name="msg">Message to show.</param>
+    public void Echo(string msg) => Console.WriteLine(msg);
 }
